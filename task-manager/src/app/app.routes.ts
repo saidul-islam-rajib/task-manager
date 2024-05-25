6a8de2f7eadda13ae9de5f297d60baf11573d486
Saidul Islam Rajib <saidul.bd@asa-international.com>
2024-05-25 13:53:25 +0600

    Add task list components

--- task-manager/src/app/app.routes.ts
+++ task-manager/src/app/app.routes.ts
@@ -1,3 +1,22 @@
-import { Routes } from '@angular/router';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { TaskListComponent } from './task-list/task-list.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+      path: '',
+      component: TaskListComponent
+    },
+    {
+      path: 'tasks',
+      component: TaskListComponent
+    }
+  
+  ];
+  
+  @NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+  })
+  export class AppRoutingModule { }
+  
